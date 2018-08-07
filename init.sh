d7c861a2db5b801c268a15e829dffe750f5ce64f
Maxim Deb Natkh <debnatkh@gmail.com>
2018-08-08 01:22:00 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /etc/nginx/sites-enabled/test.conf /home/box/web/etc/nginx.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s /etc/gunicorn.d/test /home/box/web/etc/gunicorn.conf
 sudo /etc/init.d/gunicorn restart
 sudo /etc/init.d/mysql start
 
