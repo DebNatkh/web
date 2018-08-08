6e89a1ef31a38f60fbf3f4ee3b9158a0da66b2b0
Maxim Deb Natkh <debnatkh@gmail.com>
2018-08-09 01:54:02 +0300

    updated hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,15 @@
+# def app(environ, start_response):
+#     start_response("200 OK", [
+#         ("Content-Type", "text/plain"),
+#         ("Content-Length", str(len(data)))
+#     ])
+#     return iter([data])
+
 def app(environ, start_response):
-    data = "\n".join(environ.get('QUERY_STRING').split("&"))
-    start_response("200 OK", [
-        ("Content-Type", "text/plain"),
-        ("Content-Length", str(len(data)))
-    ])
-    return iter([data])
+	status = '200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	body = '\n'.join(environ.get('QUERY_STRING').split("&"))
+	start_response(status, headers )
+	return [ body ]
