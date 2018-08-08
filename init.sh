490f476275b44906858d65cc8ec4a7766ba7be35
Maxim Deb Natkh <debnatkh@gmail.com>
2018-08-09 00:58:00 +0300

    Task 2

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/hello.py
+
 sudo /etc/init.d/nginx restart
+sudo /etc/init.d/gunicorn restart
+# ﻿sudo /etc/init.d/mysql start﻿
